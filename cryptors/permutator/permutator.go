9b46d4113ef85a4a6f5df5ad0392b5123abd854c
Bill Allie <bill.allie@defiant.mug.org>
2019-07-08 01:13:47 +0100

    Modified Permutator and Rotor to export items so they can be marhalled and unmarshalled.

--- cryptors/permutator/permutator.go
+++ cryptors/permutator/permutator.go
@@ -14,86 +14,86 @@ import (
 // cycle describes a cycle for the permutator so it can adjust the permutation
 // table used to permutate the block.  TNT currently uses 4 cycles to rearrange
 type cycle struct {
-	start   int // The starting point (into randp) for this cycle.
-	length  int // The length of the cycle.
-	current int // The point in the cycle [0 .. cycle.length-1] to start
+	Start   int // The starting point (into randp) for this cycle.
+	Length  int // The length of the cycle.
+	Current int // The point in the cycle [0 .. cycle.length-1] to start
 }
 
 // Permutator is a type that defines a permutation cryptor in TNT.
 type Permutator struct {
-	currentState  int                                     // Current number of cycles for this permutator.
-	maximalStates int                                     // Maximum number of cycles this permutator can have before repeating.
-	cycles        [cryptors.NumberPermutationCycles]cycle // Cycles ordered by the current permutation.
-	randp         [cryptors.CypherBlockSize]byte          // Values 0 - 255 in a random order.
+	CurrentState  int                                     // Current number of cycles for this permutator.
+	MaximalStates int                                     // Maximum number of cycles this permutator can have before repeating.
+	Cycles        [cryptors.NumberPermutationCycles]cycle // Cycles ordered by the current permutation.
+	Randp         [cryptors.CypherBlockSize]byte          // Values 0 - 255 in a random order.
 	bitPerm       [cryptors.CypherBlockSize]byte          // Permutation table created from randp.
 }
 
 // New creates a permutator and initializes it
 func New(cycleSizes []int, randp *[cryptors.CypherBlockSize]byte) *Permutator {
 	var p Permutator
-	p.randp = *randp
+	p.Randp = *randp
 
 	for i := range cycleSizes {
-		p.cycles[i].length = cycleSizes[i]
-		p.cycles[i].current = 0
+		p.Cycles[i].Length = cycleSizes[i]
+		p.Cycles[i].Current = 0
 		// Adjust the start to reflect the lenght of the previous cycles
 		if i == 0 { // no previous cycle so start at 0
-			p.cycles[i].start = 0
+			p.Cycles[i].Start = 0
 		} else {
-			p.cycles[i].start = p.cycles[i-1].start + p.cycles[i-1].length
+			p.Cycles[i].Start = p.Cycles[i-1].Start + p.Cycles[i-1].Length
 		}
 	}
 
-	p.currentState = 0
-	p.maximalStates = p.cycles[0].length * p.cycles[1].length * p.cycles[2].length * p.cycles[3].length
+	p.CurrentState = 0
+	p.MaximalStates = p.Cycles[0].Length * p.Cycles[1].Length * p.Cycles[2].Length * p.Cycles[3].Length
 	p.cycle()
 	return &p
 }
 
 // Update the permutator with a new initialCycles and randp
 func (p *Permutator) Update(cycleSizes []int, randp *[cryptors.CypherBlockSize]byte) {
-	p.randp = *randp
+	p.Randp = *randp
 
 	for i := range cycleSizes {
-		p.cycles[i].length = cycleSizes[i]
-		p.cycles[i].current = 0
+		p.Cycles[i].Length = cycleSizes[i]
+		p.Cycles[i].Current = 0
 		// Adjust the start to reflect the lenght of the previous cycles
 		if i == 0 { // no previous cycle so start at 0
-			p.cycles[i].start = 0
+			p.Cycles[i].Start = 0
 		} else {
-			p.cycles[i].start = p.cycles[i-1].start + p.cycles[i-1].length
+			p.Cycles[i].Start = p.Cycles[i-1].Start + p.Cycles[i-1].Length
 		}
 	}
 
-	p.currentState = 0
-	p.maximalStates = p.cycles[0].length * p.cycles[1].length * p.cycles[2].length * p.cycles[3].length
+	p.CurrentState = 0
+	p.MaximalStates = p.Cycles[0].Length * p.Cycles[1].Length * p.Cycles[2].Length * p.Cycles[3].Length
 	p.cycle()
 }
 
 // cycle bitPerm to it's next state.
 func (p *Permutator) nextState() {
-	for _, val := range p.cycles {
-		val.current = (val.current + 1) % val.length
+	for _, val := range p.Cycles {
+		val.Current = (val.Current + 1) % val.Length
 	}
 
-	p.currentState = (p.currentState + 1) % p.maximalStates
+	p.CurrentState = (p.CurrentState + 1) % p.MaximalStates
 	p.cycle()
 }
 
 func (p *Permutator) cycle() {
 	var wg sync.WaitGroup
 
-	for _, val := range p.cycles {
+	for _, val := range p.Cycles {
 		wg.Add(1)
 
 		go func(cycle []byte, sIdx int, length int) {
 			defer wg.Done()
 
 			for _, val := range cycle {
-				p.bitPerm[val] = p.randp[cycle[sIdx]]
+				p.bitPerm[val] = p.Randp[cycle[sIdx]]
 				sIdx = (sIdx + 1) % length
 			}
-		}(p.randp[val.start:val.start+val.length], val.current, val.length)
+		}(p.Randp[val.Start:val.Start+val.Length], val.Current, val.Length)
 	}
 
 	wg.Wait()
@@ -104,11 +104,11 @@ func (p *Permutator) cycle() {
 func (p *Permutator) SetIndex(idx *big.Int) {
 	q := new(big.Int)
 	r := new(big.Int)
-	q, r = q.DivMod(idx, big.NewInt(int64(p.maximalStates)), r)
-	p.currentState = int(r.Int64())
+	q, r = q.DivMod(idx, big.NewInt(int64(p.MaximalStates)), r)
+	p.CurrentState = int(r.Int64())
 
-	for _, val := range p.cycles {
-		val.current = p.currentState % val.length
+	for _, val := range p.Cycles {
+		val.Current = p.CurrentState % val.Length
 	}
 
 	p.cycle()
@@ -154,18 +154,18 @@ func (p *Permutator) String() string {
 	var output bytes.Buffer
 	output.WriteString(fmt.Sprint("\tSetCycles([...]int8{"))
 
-	for _, v := range p.cycles[0 : cryptors.NumberPermutationCycles-1] {
-		output.WriteString(fmt.Sprintf("%d, ", v.length))
+	for _, v := range p.Cycles[0 : cryptors.NumberPermutationCycles-1] {
+		output.WriteString(fmt.Sprintf("%d, ", v.Length))
 	}
 
-	output.WriteString(fmt.Sprintf("%d})\n", p.cycles[cryptors.NumberPermutationCycles-1].length))
+	output.WriteString(fmt.Sprintf("%d})\n", p.Cycles[cryptors.NumberPermutationCycles-1].Length))
 	output.WriteString(fmt.Sprint("\tSetCurrent([...]int8{"))
 
-	for _, v := range p.cycles[0 : cryptors.NumberPermutationCycles-1] {
-		output.WriteString(fmt.Sprintf("%d, ", v.current))
+	for _, v := range p.Cycles[0 : cryptors.NumberPermutationCycles-1] {
+		output.WriteString(fmt.Sprintf("%d, ", v.Current))
 	}
 
-	output.WriteString(fmt.Sprintf("%d})\n", p.cycles[cryptors.NumberPermutationCycles-1].current))
+	output.WriteString(fmt.Sprintf("%d})\n", p.Cycles[cryptors.NumberPermutationCycles-1].Current))
 	output.WriteString(fmt.Sprintf(""))
 	output.WriteString(fmt.Sprintf(""))
 	output.WriteString(fmt.Sprint("\tSetRandp([...]byte{\n"))
@@ -174,14 +174,14 @@ func (p *Permutator) String() string {
 		output.WriteString("\t\t")
 
 		if i != (cryptors.CypherBlockSize - 16) {
-			for _, k := range p.randp[i : i+16] {
+			for _, k := range p.Randp[i : i+16] {
 				output.WriteString(fmt.Sprintf("%d, ", k))
 			}
 		} else {
-			for _, k := range p.randp[i : i+15] {
+			for _, k := range p.Randp[i : i+15] {
 				output.WriteString(fmt.Sprintf("%d, ", k))
 			}
-			output.WriteString(fmt.Sprintf("%d})", p.randp[i+15]))
+			output.WriteString(fmt.Sprintf("%d})", p.Randp[i+15]))
 		}
 
 		output.WriteString("\n")
--- cryptors/rotor/rotor.go
+++ cryptors/rotor/rotor.go
@@ -11,43 +11,43 @@ import (
 )
 
 type Rotor struct {
-	start   int
-	size    int
-	step    int
-	current int
-	rotor   []byte
+	Start   int
+	Size    int
+	Step    int
+	Current int
+	Rotor   []byte
 }
 
 func New(size, start, step int, rotor []byte) *Rotor {
 	var r Rotor
-	r.start, r.current = start, start
-	r.size = size
-	r.step = step
-	r.rotor = rotor
+	r.Start, r.Current = start, start
+	r.Size = size
+	r.Step = step
+	r.Rotor = rotor
 	var i, j uint
 	for i = 0; i < 256; i += 1 {
-		j = uint(r.size) + i
-		if bitops.GetBit(r.rotor, i) {
-			bitops.SetBit(r.rotor, j)
+		j = uint(r.Size) + i
+		if bitops.GetBit(r.Rotor, i) {
+			bitops.SetBit(r.Rotor, j)
 		} else {
-			bitops.ClrBit(r.rotor, j)
+			bitops.ClrBit(r.Rotor, j)
 		}
 	}
 	return &r
 }
 
 func (r *Rotor) Update(size, start, step int, rotor []byte) {
-	r.start, r.current = start, start
-	r.size = size
-	r.step = step
-	r.rotor = rotor
+	r.Start, r.Current = start, start
+	r.Size = size
+	r.Step = step
+	r.Rotor = rotor
 	var i, j uint
 	for i = 0; i < 256; i += 1 {
-		j = uint(r.size) + i
-		if bitops.GetBit(r.rotor, i) {
-			bitops.SetBit(r.rotor, j)
+		j = uint(r.Size) + i
+		if bitops.GetBit(r.Rotor, i) {
+			bitops.SetBit(r.Rotor, j)
 		} else {
-			bitops.ClrBit(r.rotor, j)
+			bitops.ClrBit(r.Rotor, j)
 		}
 	}
 }
@@ -55,15 +55,15 @@ func (r *Rotor) Update(size, start, step int, rotor []byte) {
 func (rotor *Rotor) SetIndex(idx *big.Int) {
 	// Special case if idx == 0
 	if idx.Sign() == 0 {
-		rotor.current = rotor.start
+		rotor.Current = rotor.Start
 	} else {
 		p := new(big.Int)
 		q := new(big.Int)
 		r := new(big.Int)
-		p = p.Mul(idx, new(big.Int).SetInt64(int64(rotor.step)))
-		p = p.Add(p, new(big.Int).SetInt64(int64(rotor.start)))
-		q, r = q.DivMod(p, new(big.Int).SetInt64(int64(rotor.size)), r)
-		rotor.current = int(r.Int64())
+		p = p.Mul(idx, new(big.Int).SetInt64(int64(rotor.Step)))
+		p = p.Add(p, new(big.Int).SetInt64(int64(rotor.Start)))
+		q, r = q.DivMod(p, new(big.Int).SetInt64(int64(rotor.Size)), r)
+		rotor.Current = int(r.Int64())
 	}
 }
 
@@ -71,19 +71,19 @@ func (rotor *Rotor) Index() *big.Int {
 	return nil
 }
 
-func (rotor *Rotor) Size() int {
-	return rotor.size
-}
+// func (rotor *Rotor) Size() int {
+// 	return rotor.Size
+// }
 
-func (rotor *Rotor) Rotor() []byte {
-	return rotor.rotor
-}
+// func (rotor *Rotor) Rotor() []byte {
+// 	return rotor.Rotor
+// }
 
 func (r *Rotor) Apply_F(blk *[cryptors.CypherBlockBytes]byte) *[cryptors.CypherBlockBytes]byte {
 	var res [cryptors.CypherBlockBytes]byte
 	ress := res[:]
-	rotor := r.rotor
-	idx := r.current
+	rotor := r.Rotor
+	idx := r.Current
 
 	for cnt := 0; cnt < cryptors.CypherBlockSize; cnt++ {
 		if bitops.GetBit(rotor, uint(idx)) {
@@ -93,15 +93,15 @@ func (r *Rotor) Apply_F(blk *[cryptors.CypherBlockBytes]byte) *[cryptors.CypherB
 		idx++
 	}
 
-	r.current = (r.current + r.step) % r.size
+	r.Current = (r.Current + r.Step) % r.Size
 	return cryptors.AddBlock(blk, &res)
 }
 
 func (r *Rotor) Apply_G(blk *[cryptors.CypherBlockBytes]byte) *[cryptors.CypherBlockBytes]byte {
 	var res [cryptors.CypherBlockBytes]byte
 	ress := res[:]
-	rotor := r.rotor[:]
-	idx := r.current
+	rotor := r.Rotor[:]
+	idx := r.Current
 
 	for cnt := 0; cnt < cryptors.CypherBlockSize; cnt++ {
 		if bitops.GetBit(rotor, uint(idx)) {
@@ -111,25 +111,26 @@ func (r *Rotor) Apply_G(blk *[cryptors.CypherBlockBytes]byte) *[cryptors.CypherB
 		idx++
 	}
 
-	r.current = (r.current + r.step) % r.size
+	r.Current = (r.Current + r.Step) % r.Size
 	return cryptors.SubBlock(blk, &res)
 }
 
 func (rotor *Rotor) String() string {
 	var output bytes.Buffer
-	output.WriteString(fmt.Sprintf("rotor.New(%d, %d, %d,[...]byte{\n",
-		rotor.size, rotor.start, rotor.step))
-	for i := 0; i < 1024; i += 16 {
+	rotorLen := len(rotor.Rotor)
+	output.WriteString(fmt.Sprintf("rotor.New(%d, %d, %d,[]byte{\n",
+		rotor.Size, rotor.Start, rotor.Step))
+	for i := 0; i < rotorLen; i += 16 {
 		output.WriteString("\t")
-		if i != 1008 {
-			for _, k := range rotor.rotor[i : i+16] {
+		if i != rotorLen-16 {
+			for _, k := range rotor.Rotor[i : i+16] {
 				output.WriteString(fmt.Sprintf("%d, ", k))
 			}
 		} else {
-			for _, k := range rotor.rotor[i : i+15] {
+			for _, k := range rotor.Rotor[i : i+15] {
 				output.WriteString(fmt.Sprintf("%d, ", k))
 			}
-			output.WriteString(fmt.Sprintf("%d})", rotor.rotor[i+15]))
+			output.WriteString(fmt.Sprintf("%d})", rotor.Rotor[i+15]))
 		}
 		output.WriteString("\n")
 	}
